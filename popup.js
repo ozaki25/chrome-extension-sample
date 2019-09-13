7267990e0bee39f396d9c3c549bc40d2f80322f9
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-14 03:49:21 +0900

    append hello script

--- popup.js
+++ popup.js
@@ -9,7 +9,16 @@ changeColor.onclick = function(element) {
   let color = element.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";',
+      code: `
+      document.body.style.backgroundColor = "${color}";
+      function appendHello() {
+        const h1 = document.createElement('h1');
+        const text = document.createTextNode('Hello');
+        h1.appendChild(text);
+        document.body.appendChild(h1);
+      }
+      appendHello();
+      `,
     });
   });
 };
